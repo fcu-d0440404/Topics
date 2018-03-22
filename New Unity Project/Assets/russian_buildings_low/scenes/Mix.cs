5a3937ecd3a911124be36030091687ba5ed7165b
fcu-d0440404 <noyourdocm@gmail.com>
2018-03-22 21:51:42 +0800

    0322

---
+++ New Unity Project/Assets/russian_buildings_low/scenes/Mix.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.active = false;
+        }
+
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        transform.gameObject.active = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
